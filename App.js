e874a8f628716026dfa1597e1c0854c9e57bf7de
Advaith.V.A <97515865+Advaithva@users.noreply.github.com>
2023-01-14 10:17:54 +0530

    Update App.js

--- App.js
+++ App.js
@@ -8,4 +8,5 @@ function closeNav() {
 
 function INST() {
   alert("Welcome to the web page about Vistadome Coach in India. Hope You Enjoy It!"); 
+  alert("Instructions - In Speciality and Images sceton, hover over the images if you are using laptop and tap on the images if you are using mobile. Don't forget to give feedback if you like it.");
 }
